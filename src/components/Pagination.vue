d8523367f227d5905a43a3b0dde0f92efb19e050
Adrien Hobbs <adrien.hobbs@gmail.com>
2020-02-17 14:26:30 -0500

    update pagination logic

--- src/components/Pagination.vue
+++ src/components/Pagination.vue
@@ -1,8 +1,18 @@
 <template>
   <div class="pagination-container">
-    <div class="button previous-page" @click="prevPage">Prev</div>
-    <div class="current-page">{{ currentPage + 1 }}</div>
-    <div class="button next-page" @click="nextPage">
+    <div
+      class="button previous-page"
+      :class="{ disabled: isFirstPage }"
+      @click="updatePage(currentPage - 1)"
+    >
+      Prev
+    </div>
+    <div class="current-page">{{ currentPage + 1 }} / {{ totalPages }}</div>
+    <div
+      class="button next-page"
+      :class="{ disabled: isLastPage }"
+      @click="updatePage(currentPage + 1)"
+    >
       Next
     </div>
   </div>
@@ -27,30 +37,39 @@ export default {
     };
   },
   methods: {
-    nextPage() {
-      if (!this.isLastPage) {
-        this.currentPage = this.currentPage + 1;
-        this.$emit("pageUpdated", this.currentPage);
-      }
-    },
-    prevPage() {
-      if (!this.isFirstPage) {
-        this.currentPage = this.currentPage - 1;
-        this.$emit("pageUpdated", this.currentPage);
-      }
+    updatePage(page) {
+      this.currentPage = page;
+      this.$emit("pageUpdated", this.currentPage);
+      window.scrollTo(null, 0);
     }
   },
   computed: {
+    shouldUpdate() {
+      return !this.isFirstPage && !this.isLastPage;
+    },
     isFirstPage() {
       return !this.currentPage;
     },
+    totalPages() {
+      return Math.round(this.totalItems / this.itemsPerPage);
+    },
     isLastPage() {
-      return (
-        this.currentPage + 1 === Math.round(this.totalItems / this.itemsPerPage)
-      );
+      return this.currentPage + 1 === this.totalPages;
     }
   }
 };
 </script>
 
-<style></style>
+<style lang="scss">
+.pagination-container {
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  margin-bottom: 30px;
+
+  .current-page {
+    padding-left: 5px;
+    padding-right: 5px;
+  }
+}
+</style>
