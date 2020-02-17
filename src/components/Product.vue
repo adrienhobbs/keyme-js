68449271cda04b9ff2b97ffe66156435ef14d28e
Adrien Hobbs <adrien.hobbs@gmail.com>
2020-02-17 13:36:56 -0500

    display products

---
+++ src/components/Product.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="product">
+    <img :src="product.imageURL" alt="" />
+    <div class="product-name">
+      {{ product.display_name }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Product",
+  props: {
+    product: {
+      type: Object,
+      required: true
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+.product {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+
+  img {
+    margin-left: 60px;
+    margin-right: 60px;
+    max-width: 80px;
+    max-height: 200px;
+  }
+}
+</style>
--- src/components/ProductListing.vue
+++ src/components/ProductListing.vue
@@ -1,21 +1,70 @@
 <template>
   <div class="product-listing">
-    <div class="button" @click="$emit('updateView', 'address-form')">
-      Checkout
+    <div class="products-container">
+      <Product
+        v-for="product in productsToDisplay"
+        :key="product.key"
+        :product="product"
+      />
     </div>
+
+    <!-- <div class="button" @click="$emit('updateView', 'address-form')">
+      Checkout
+    </div> -->
+    <Pagination
+      @pageUpdated="page => (currentPage = page)"
+      :totalItems="products.length"
+      :itemsPerPage="productsPerPage"
+    />
   </div>
 </template>
 
 <script>
+import Product from "@/components/Product";
+import Pagination from "@/components/Pagination";
+
 export default {
   name: "ProductListing",
+  components: {
+    Product,
+    Pagination
+  },
   props: {
     products: {
       type: Array,
       required: true
     }
+  },
+  data() {
+    return {
+      currentPage: 0,
+      productsPerPage: 9
+    };
+  },
+  computed: {
+    productsToDisplay() {
+      const startingIndex = this.currentPage * this.productsPerPage;
+      return this.products.slice(
+        startingIndex,
+        startingIndex + this.productsPerPage
+      );
+    }
   }
 };
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+.products-container {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  padding-left: 15px;
+  padding-right: 15px;
+
+  @media (min-width: 768px) {
+    padding-left: 30px;
+    padding-right: 30px;
+    justify-content: space-between;
+  }
+}
+</style>
