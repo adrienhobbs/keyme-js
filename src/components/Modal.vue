225de449ddf5d55c2fa3b7d38e93726dc7d4a8b5
Adrien Hobbs <adrien.hobbs@gmail.com>
2020-02-18 13:24:53 -0500

    add basic modal component

---
+++ src/components/Modal.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="modal" v-if="visible">
+    <div class="modal-inner">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Modal",
+  props: {
+    visible: {
+      type: Boolean,
+      required: true
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+.modal {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100vh;
+  background-color: rgba(0, 0, 0, 0.4);
+}
+
+.modal-inner {
+  padding: 30px;
+  background-color: white;
+}
+</style>
